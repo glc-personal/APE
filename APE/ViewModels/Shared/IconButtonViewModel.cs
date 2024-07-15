fa941402cded317f1fa121071a19d6642300c549
glc-personal <gabriel.lopez.candales@gmail.com>
2024-07-15 00:28:07 -0400

    Added icons, a DiscreteProgressBar UserControl, an IconButton UserControl, with their respective View Models. Updated the MainWindow.

---
+++ APE/ViewModels/Shared/IconButtonViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace APE.ViewModels.Shared
+{
+    public class IconButtonViewModel : INotifyPropertyChanged
+    {
+        /*
+         * --------------------------------------------------------------------------------------------------------
+         * Private Attributes
+         * --------------------------------------------------------------------------------------------------------
+         */
+        private string iconPath { get; set; }
+        private ICommand command { get; set; }
+
+        /*
+         * --------------------------------------------------------------------------------------------------------
+         * Public Attributes
+         * --------------------------------------------------------------------------------------------------------
+         */
+        public string IconPath
+        {
+            get => iconPath;
+            set
+            {
+                iconPath = value;
+                OnPropertyChanged(nameof(IconPath));
+            }
+        }
+        public ICommand Command
+        {
+            get => command;
+            set
+            {
+                command = value;
+                OnPropertyChanged(nameof(Command));
+            }
+        }
+
+        /*
+         * --------------------------------------------------------------------------------------------------------
+         * INotifyPropertyChanged Methods
+         * --------------------------------------------------------------------------------------------------------
+         */
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
