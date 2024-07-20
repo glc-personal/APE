51c51f60b1283199864b62f3586ec919f80992f8
glc-personal <gabriel.lopez.candales@gmail.com>
2024-07-20 00:53:02 -0400

    Added an AddStepWindow for adding steps to the Assay Protocol Editor. Currently has no functionality, it is all design.

---
+++ APE/ViewModels/AddStepWindow/AddStepWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using APE.ViewModels.Shared;
+using System.ComponentModel;
+
+namespace APE.ViewModels.AddStepWindow
+{
+    public class AddStepWindowViewModel : INotifyPropertyChanged
+    {
+        /*
+         * ------------------------------------------------------------------------------------------------------
+         * Private Attributes
+         * ------------------------------------------------------------------------------------------------------
+         */
+
+        /*
+         * ------------------------------------------------------------------------------------------------------
+         * Public Attributes
+         * ------------------------------------------------------------------------------------------------------
+         */
+        public BannerViewModel MyBannerViewModel { get; set; }
+        public DescriptorViewModel MyLiquidHandlingStepsDescriptorViewModel { get; set; }
+        public DescriptorViewModel MyDurationStepsDescriptorViewModel { get; set; }
+        public DescriptorViewModel MyGantryStepsDescriptorViewModel { get; set; }
+        public DescriptorViewModel MyReaderStepsDescriptorViewModel { get; set; }
+
+        /*
+         * ------------------------------------------------------------------------------------------------------
+         * Commands
+         * ------------------------------------------------------------------------------------------------------
+         */
+
+        /*
+         * ------------------------------------------------------------------------------------------------------
+         * INotifyPropertyChanged Methods
+         * ------------------------------------------------------------------------------------------------------
+         */
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- APE/ViewModels/MainWindowViewModel.cs
+++ APE/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using APE.Commands;
 using APE.ViewModels.Shared;
+using APE.Views.AddStepWindow;
 using System;
 using System.ComponentModel;
 using System.Diagnostics;
@@ -25,6 +26,7 @@ namespace APE.ViewModels
         private ICommand playCommand;
         private ICommand pauseCommand;
         private ICommand stopCommand;
+        private ICommand addStepCommand;
         private Brush statusBarBackground;
         private Brush statusBarForeground;
 
@@ -128,6 +130,28 @@ namespace APE.ViewModels
         public bool CanExecuteStopCommand(object parameter)
         {
             // Logic for if the stop command can be execute
+            return true;
+        }
+        public ICommand AddStepCommand
+        {
+            get
+            {
+                if (addStepCommand == null)
+                {
+                    addStepCommand = new RelayCommand(ExecuteAddStepCommand, CanExecuteAddStepCommand);
+                }
+                return addStepCommand;
+            }
+        }
+        public void ExecuteAddStepCommand(object parameter)
+        {
+            // If protocol is not currently running (add ability to create a new protocol while one is running)
+            APE.Views.AddStepWindow.AddStepWindow addStepWindow = new Views.AddStepWindow.AddStepWindow();
+            addStepWindow.Show();
+        }
+        public bool CanExecuteAddStepCommand(object parameter)
+        {
+            // Logic for if the add step command can be executed
             return true;
         }
 
