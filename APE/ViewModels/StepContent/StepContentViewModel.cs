89d59879b74a134004a5a6eb4de0345df7ea84e8
glc-personal <gabriel.lopez.candales@gmail.com>
2024-07-23 09:42:48 -0400

    Small update

--- APE/ViewModels/StepContent/StepContentViewModel.cs
+++ APE/ViewModels/StepContent/StepContentViewModel.cs
@@ -12,5 +12,67 @@ namespace APE.ViewModels.StepContent
          */
         public BannerViewModel MyBannerViewModel { get; set; }
         public DescriptorViewModel MyDescriptorViewModel { get; set; }
+
+        /*
+         * ------------------------------------------------------------------------------------------------------
+         * Private Attributes
+         * ------------------------------------------------------------------------------------------------------
+         */
+        private string stepName;
+        private string batch;
+        private string sampleType;
+        private int volume;
+        private bool requiresNewTips;
+
+        /*
+         * ------------------------------------------------------------------------------------------------------
+         * Public Attributes
+         * ------------------------------------------------------------------------------------------------------
+         */
+        public string StepName
+        {
+            get => stepName;
+            set
+            {
+                stepName = value;
+                OnPropertyChanged(nameof(StepName));
+            }
+        }
+        public string Batch
+        {
+            get => batch;
+            set
+            {
+                batch = value;
+                OnPropertyChanged(nameof(Batch));
+            }
+        }
+        public string SampleType
+        {
+            get => sampleType;
+            set
+            {
+                sampleType = value;
+                OnPropertyChanged(nameof(SampleType));
+            }
+        }
+        public int Volume
+        {
+            get => volume;
+            set
+            {
+                volume = value;
+                OnPropertyChanged(nameof(Volume));
+            }
+        }
+        public bool RequiresNewTips
+        {
+            get => requiresNewTips;
+            set
+            {
+                requiresNewTips = value;
+                OnPropertyChanged(nameof(RequiresNewTips));
+            }
+        }
     }
 }
